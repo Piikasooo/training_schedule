b9be20b7639baf8aa6f7b08ffd24f4d66e253107
Your Name <artger@ukr.net>
2020-03-27 18:48:37 +0200

    add test for users/test

--- users/tests.py
+++ users/tests.py
@@ -1,15 +1,37 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
 
 class RegistrationTestCase(APITestCase):
-
     def test_registration(self):
         # registration new user
-        data = {'username': 'test_user', 'email': 'asdf@test.com', 'password': 'qwerty1234'}
-        response = self.client.post('http://127.0.0.1:8000/api/users/', data)
+        data = {'username': 'pika', 'email': 'asdf@test.com', 'password': '1'}
+        response = self.client.post('http://127.0.0.1:8000/users/users/', data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         # try register exiting user
-        response = self.client.post('http://127.0.0.1:8000/api/users/', data)
+        response = self.client.post('http://127.0.0.1:8000/users/users/', data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class PermissionTestCase(APITestCase):
+
+    def setUp(self):
+        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
+        self.user = User.objects.create_user('pika', 'pika@snow.com', '1')
+
+    def test_unauthorized(self):
+        response = self.client.get('http://127.0.0.1:8000/users/users/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_forbiden(self):
+        self.client.login(username='pika', password='1')
+        response = self.client.get('http://127.0.0.1:8000/users/users/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_admin_permission(self):
+        self.client.login(username='john', password='johnpassword')
+        response = self.client.get('http://127.0.0.1:8000/users/users/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
