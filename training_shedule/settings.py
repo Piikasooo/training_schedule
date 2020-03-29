42d46991acc0fcad6530c646226da7a677b365d2
Your Name <artger@ukr.net>
2020-03-29 09:28:00 +0300

    heroku deploy test 3

--- training_shedule/settings.py
+++ training_shedule/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import django_heroku
 import os
 from environs import Env
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['training_shedule.herokuapp.com']
 
 
 
@@ -147,4 +148,7 @@ REST_FRAMEWORK = {
 
 # Acivate Dgango-Heroku.
 django_heroku.settings(locals())
+
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
